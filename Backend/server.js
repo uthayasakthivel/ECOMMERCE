b241c0372b961681a41a34c022057c4bca8efe5b
uthayasakthivel <uthayasakthivel8@gmail.com>
2024-11-08 13:26:54 +0530

    create empty folders named router, models, controllers and add dotenv and middleware and create empty productRoutes.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
 
 // Express App
 const app = express();
+
+// Dotenv Config
+dotenv.config();
+
+// MiddleWare
+app.use((req, res, next) => {
+  console.log("check dummy middleware");
+  next();
+});
 
 // Routes
 app.get("/", (req, res) => {
   res.json({ msg: "hello world!" });
 });
 
+// PORT Number
+const port = process.env.PORT;
+
 // Listening Requests
-app.listen(3000, () => {
-  console.log("Backend Connected ✌️");
+app.listen(port || 3000, () => {
+  console.log(`Backend Connected ✌️ at port ${port}`);
 });
